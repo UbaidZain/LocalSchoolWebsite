24deae33391d40bff60bf4f2bd800bca0388dd75
Ubaid Zain <uk348152@gmail.com>
2023-11-14 21:55:11 +0500

    Added initial files and nav-bar

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const navMenuContainer =
+  document.getElementsByClassName("nav-menu-container")[0];
+const navMenu = document.getElementsByClassName("nav-menu")[0];
+
+hamburger.addEventListener("click", (e) => {
+  console.log("hello");
+  e.target.classList.toggle("is-active");
+  navMenuContainer.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+  e.target.firstElementChild.classList.toggle("fa-xmark");
+});
